46b381404478096f41a19e14025bb574e4ae14dd
jacobschilders <jschilders@anderson.edu>
2020-11-09 15:31:25 -0500

    Delete

--- Restaurant_Rater/Controllers/RestaurantController.cs
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -35,6 +36,32 @@ namespace Restaurant_Rater.Controllers
                 return RedirectToAction("Index");
             }
             return View(restaurant);
+        }
+
+        //Get: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //Post: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            _context.Restaurants.Remove(restaurant);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
