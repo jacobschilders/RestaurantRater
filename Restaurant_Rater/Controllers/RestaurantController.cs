828a27a76f64999d3352dc1a34abe5f49f4a0ab5
jacobschilders <jschilders@anderson.edu>
2020-11-09 17:11:52 -0500

    Edit

--- Restaurant_Rater/Controllers/RestaurantController.cs
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using Restaurant_Rater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -63,5 +64,36 @@ namespace Restaurant_Rater.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //Get: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        
+        //Post: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(restaurant).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+    
+
     }
 }
