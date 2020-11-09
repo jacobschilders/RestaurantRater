2492087b19792490dfbdf8f5434d4e71233ed010
jacobschilders <jschilders@anderson.edu>
2020-11-09 13:40:37 -0500

    model setup

---
+++ Restaurant_Rater/Models/Restaurant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant_Rater.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Rating { get; set; }
+
+
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
